53e95b310fdb6bcb57c7fc2220ca5d2dd7175844
gurbanovcavad <gurbanovcavad7@gmail.com>
2025-04-04 23:47:32 +0400

    Add Features

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("", views.index, name='index'),
+    path("<int:id>/", views.flight, name='flight'),
+    path("<int:id>/book/", views.book, name='book')
 ]
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Flight, Passenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    context = {"flights": Flight.objects.all()}
+    return render(request, 'flights/index.html', context)
+
+def flight(request, id):
+    f = Flight.objects.get(pk=id)
+    context = {'flight': f, 'passengers': f.passengers.all(), 'non_passengers': Passenger.objects.exclude(flights=f).all()}
+    return render(request, 'flights/flight.html', context)
+
+def book(request, id):
+    if request.method == 'POST':
+        flight = Flight.objects.get(pk=id)
+        passenger = Passenger.objects.get(pk=int(request.POST['passenger']))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=[id, ]))
